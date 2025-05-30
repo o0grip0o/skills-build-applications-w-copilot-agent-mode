239535553b94d6d6803aaa7682d483954505ab6a
Ossie <ossie.bodero@outlook.com>
2025-05-30 14:45:08 +0000

    Add ObjectIdField as primary key for models

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,24 +1,30 @@
 from djongo import models
+from djongo.models import ObjectIdField
 
 class User(models.Model):
+    id = ObjectIdField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
 class Team(models.Model):
+    id = ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255)
     members = models.JSONField()
 
 class Activity(models.Model):
+    id = ObjectIdField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
     duration = models.IntegerField()
     date = models.DateField()
 
 class Leaderboard(models.Model):
+    id = ObjectIdField(primary_key=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     points = models.IntegerField()
 
 class Workout(models.Model):
+    id = ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
